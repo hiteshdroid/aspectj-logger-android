a2baae3aa7aa002d59b96d3740e8e60559b1b1ba
hiteshandroid <hitesh.das@olx.com>
2019-08-07 11:18:00 +0530

    Added Kotlin coverage

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,15 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        launchActivity();
-
-        SampleKotlinActivity sampleKotlinActivity = new SampleKotlinActivity();
-        sampleKotlinActivity.callFromOutside();
+        findViewById(R.id.button).setOnClickListener(v -> launchActivity());
     }
 
     private void launchActivity() {
-        //Intent intent = new Intent(this, SampleKotlinActivity.class);
-        //startActivity(intent);
+        Intent intent = new Intent(this, SampleKotlinActivity.class);
+        startActivity(intent);
     }
 }
